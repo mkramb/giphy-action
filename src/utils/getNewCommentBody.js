64f7cb0a131cd053cc1fa09ef84e8dd1503a44ce
Mitja Kramberger <mitja.kramberger@gmail.com>
2019-09-29 23:13:23 +0100

    test 2

--- src/utils/getNewCommentBody.js
+++ src/utils/getNewCommentBody.js
@@ -6,5 +6,16 @@ module.exports = async function getNewCommentBody(giphy, lastComment) {
   const response = await searchByQuery(giphy, query);
   const imageUrl = getBestImage(response);
 
+  core.info(
+    JSON.stringify(
+      {
+        response,
+        imageUrl
+      },
+      null,
+      2
+    )
+  );
+
   return `![${query}](${imageUrl})`;
 };
