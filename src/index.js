b0efb823f2de42eedc2fc222e56de1be485f2f66
Mitja Kramberger <mitja.kramberger@gmail.com>
2019-09-29 22:44:55 +0100

    Fixing 1

--- src/index.js
+++ src/index.js
@@ -2,22 +2,24 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const giphy = require("giphy-js-sdk-core");
 
-const getLastComment = require("./utils/getLastComment");
-const getNewCommentBody = require("./utils/getNewCommentBody");
-const postComment = require("./utils/postComment");
+// const getLastComment = require("./utils/getLastComment");
+// const getNewCommentBody = require("./utils/getNewCommentBody");
+// const postComment = require("./utils/postComment");
 
 async function run() {
   try {
     const giphySecret = core.getInput("giphy-secret");
     const githubSecret = core.getInput("github-secret");
 
-    const octokit = new github.GitHub(githubSecret);
-    const lastComment = getLastComment(octokit);
+    console.log(JSON.stringify(github.context, null, 2));
 
-    if (lastComment) {
-      const newCommentBody = getNewCommentBody(giphy(giphySecret));
-      await postComment(octokit, lastComment, newCommentBody);
-    }
+    // const octokit = new github.GitHub(githubSecret);
+    // const lastComment = getLastComment(octokit);
+
+    // if (lastComment) {
+    //   const newCommentBody = getNewCommentBody(giphy(giphySecret));
+    //   await postComment(octokit, lastComment, newCommentBody);
+    // }
   } catch (error) {
     core.error(error);
     core.setFailed(error.message);
