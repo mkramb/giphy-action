db48e29e9139dd1262126c5b6067d760c08a98f7
Mitja Kramberger <mitja.kramberger@gmail.com>
2019-09-29 23:11:34 +0100

    test 1

--- src/utils/updateComment.js
+++ src/utils/updateComment.js
@@ -1,3 +1,4 @@
+const core = require("@actions/core");
 const github = require("@actions/github");
 
 module.exports = async function updateComment(
@@ -5,10 +6,13 @@ module.exports = async function updateComment(
   lastComment,
   newCommentBody
 ) {
-  await octokit.issues.updateComment({
+  const payload = {
     owner: github.context.actor,
     repo: github.context.payload.repository.name,
     comment_id: lastComment.id,
     body: newCommentBody
-  });
+  };
+
+  core.info(JSON.stringify(payload, null, 2));
+  await octokit.issues.updateComment(payload);
 };
